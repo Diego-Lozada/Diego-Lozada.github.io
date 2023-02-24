374d579791337a321db8106626d77a5a06fcf9a2
Diego-Lozada <111712929+Diego-Lozada@users.noreply.github.com>
2023-02-24 11:07:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*window.sr = ScrollReveal();
+
+
+  /*sr.reveal('.banner_title', {
+    duration: 3000,
+       origin: 'top',
+    distance: '100px',
+  });
+
+ sr.reveal('.trabajotitulo', {
+    duration: 2000,
+       origin: 'top',
+    distance: '-100px',
+  });*/
+
+ /*   sr.reveal('.carrusel-items', {
+    duration: 2000,
+    
+  });
+*/
+  
+
+  
